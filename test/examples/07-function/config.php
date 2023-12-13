749c93f03e885dd7c3e48552860582e7aca0f454
bizlal <bilal@limelight.io>
2023-12-13 10:11:54 -0500

    f

--- test/examples/07-function/config.php
+++ test/examples/07-function/config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'ABvUxzDNz8uXgQBRQfv9hGNUUbXBlyc5afV8tXYOQe5TCt9r4K',
-    'client_secret' => 'BaEJlTMkb27eKPOpaPVjHo9PFOhIV5XLPiECPABV',
+    'client_id' => 'ABHMcSCef5OhmCR4i0oFdRyqddunDY2shw5d9bxxh4LQ7o7TTZ',
+    'client_secret' => 'IvsnJh0jjjID8f3hm88adLOF7kb2lEAh575gdais',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'https://php-b7r1z49oc-bilal-limelightio.vercel.app/callback',
 )
